e10fa37fce3905e23243c440483a3446c42ca463
Vatsal Sharma <vatsalsharma376@gmail.com>
2017-02-15 09:55:32 +0530

    Create COCI06 #2 - R2.cpp

---
+++ COCI06 #2 - R2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define pb(x) push_back(x);
+#define in(y) insert(y);
+#define tt(t) while(t--)
+#define itr ::iterator it;
+#define ll long long
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<ii>
+#define si set<int>                      
+#define msi map<string, int>
+#define new cout<<endl
+#define ff(n) for(int i=0;i<n;i++)
+#define all(v) sort(v.begin(),v.end())
+
+using namespace std;
+
+int main(){
+    int r,s;
+    cin>>r>>s;
+    cout<<2*s-r<<endl;
+    
+    return 0;
+}
