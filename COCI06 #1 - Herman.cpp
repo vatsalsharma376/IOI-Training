997eb6ae64ae02df6135520f290c2310f5549a96
Vatsal Sharma <vatsalsharma376@gmail.com>
2017-02-15 09:51:42 +0530

    Create COCI06 #1 - Herman.cpp

---
+++ COCI06 #1 - Herman.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define pb(x) push_back(x);
+#define in(y) insert(y);
+#define tt(t) while(t--)
+#define itr ::iterator it;
+#define ll long long
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<ii>
+#define si set<int>                      
+#define msi map<string, int>
+#define new cout<<endl
+#define ff(n) for(int i=0;i<n;i++)
+#define all(v) sort(v.begin(),v.end())
+#define PI acos(-1)
+
+using namespace std;
+
+int main(){
+    int r;
+    cin>>r;
+    cout<<fixed<<setprecision(8)<<PI*r*r<<endl;
+    cout<<fixed<<setprecision(8)<<(long double)r*r*2<<endl;
+    
+    return 0;
+}
